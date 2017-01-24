e422808c2d08e14fdaa688068742b0e52df18b15
liaoruiwen <liaorw@dibugroup.cn>
2017-01-24 15:28:37 +0800

    搭建基础设置，配置公用类型

--- app/src/main/java/com/lc/liao/mygithub/activity/test/TestActivity.java
+++ app/src/main/java/com/lc/liao/mygithub/activity/test/TestActivity.java
@@ -1,5 +1,10 @@
 package com.lc.liao.mygithub.activity.test;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
+
+import com.lc.liao.mygithub.R;
 import com.lc.liao.mygithub.activity.base.BaseActivity;
 
 /**
@@ -7,13 +12,21 @@ import com.lc.liao.mygithub.activity.base.BaseActivity;
  */
 
 public class TestActivity extends BaseActivity{
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+
     @Override
     protected void initView() {
+        mDrawerLayout = getViewById(R.id.id_drawer_layout);
+        mNavigationView = getViewById(R.id.id_nv_menu);
+
+        Toolbar toolbar = getViewById(R.id.id_toolbar);
+        setSupportActionBar(toolbar);
 
     }
 
     @Override
     protected int getLayoutId() {
-        return 0;
+        return R.layout.activity_test;
     }
 }
